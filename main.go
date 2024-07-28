d1da5f5de44e9893467e73dd6366193c3b362fc6
andi faizal <andifaizal@MacBook-Pro-andi.local>
2024-07-29 00:40:08 +0700

    learn golang

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+var classes string = "Andi 9B"
+
+func main(){
+   var classes string = "Andi 9B 7"
+
+	// var name string = "name"
+	// var nameOne ="hello"
+	fmt.Println(classes)
+}
